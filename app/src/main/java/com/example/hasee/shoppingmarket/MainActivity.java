26f14f11eb76bdc2a7e7a7a3f3d40a7f5416c4ee
hasee <1774383254@qq.com>
2017-02-22 22:19:47 +0800

    修复左侧菜单

--- app/src/main/java/com/example/hasee/shoppingmarket/MainActivity.java
+++ app/src/main/java/com/example/hasee/shoppingmarket/MainActivity.java
@@ -56,8 +56,8 @@ public class MainActivity extends AppCompatActivity implements LeftMenuAdapter.o
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
         initData();
+        initView();
         initAdapter();
     }
 
